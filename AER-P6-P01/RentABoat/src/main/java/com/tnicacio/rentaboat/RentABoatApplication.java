7fedad5f4a759a60640a5b7eb40e21b60fdaa185
tnicacio <nicacio.t92@gmail.com>
2021-09-16 21:15:21 -0300

    RentABoat initial commit

---
+++ AER-P6-P01/RentABoat/src/main/java/com/tnicacio/rentaboat/RentABoatApplication.java
@@ -0,0 +1,15 @@
+package com.tnicacio.rentaboat;
+
+public class RentABoatApplication {
+
+    /*
+    Uma empresa de locação de barcos trabalha com os seguintes modelos de embarcações:
+    bateira, iate, canoa, jangada e barco a vela. Os dois primeiros modelos usam motor, os dois
+    seguintes são movimentados a remos e o último à vela. Usando o padrão strategy crie e
+    implemente um modelo de classes que, dado um determinado barco, informe como ele está se
+    movimentando.
+    */
+    public static void main(String... args) {
+        System.out.println("Olá");
+    }
+}
